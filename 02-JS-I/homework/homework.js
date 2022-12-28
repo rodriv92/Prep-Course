d7cd1c229026aaba4b88349f08d8c5a55b5b7861
Rodrigo Vallejos <rvallejos1992@gmail.com>
2022-12-27 21:09:14 -0300

    Esto es un commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,7 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = "Esto es una cadena";
+const nuevaString = "Estoy aprendiendo Javascript"; _
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = 30;
